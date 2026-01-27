503a5531fa067d480d54b8340126f203c83edc20
Daniel <danielzmalka@gmail.com>
2026-01-27 12:16:57 +0200

    Learned and added logging middleware and admin control

--- projects/node-server-01/app.js
+++ projects/node-server-01/app.js
@@ -3,6 +3,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+app.use((req, res, next) => {
+    const time = new Date().toLocaleString();
+    console.log(`[${time}] ${req.method} request to: ${req.url}`);
+
+    if (req.query.admin === 'true') {
+        console.log("⚠️ Access Control: Admin authorized.");
+        next();
+    } else {
+        console.log("🚫 Access Control: Unauthorized attempt blocked.");
+        res.status(403).send('<h1>403 Forbidden</h1><p>You need admin privileges to access this server.</p>');
+    }
+});
+
 app.get('/', (req, res) => {
     res.send('Server is Up and Running with ENV!');
 });
